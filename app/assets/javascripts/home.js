f73016696b3e492ec61bab93265a40ea04ff3dca
dave <djungst@gmail.com>
2016-02-17 19:48:36 -0700

    dont load image until loaded

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,11 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready( function() {
+  var img = new Image();
+      img.src = 'assets/cover-compressor.png';
+
+  var int = setInterval(function() {
+      if (img.complete) {
+          clearInterval(int);
+          document.getElementById('main').style.backgroundImage = 'url(' + img.src + ')';
+      }
+  }, 50);
+});
