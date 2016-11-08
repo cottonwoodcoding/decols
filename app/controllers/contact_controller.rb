99f602ca3ed44fb74117667e34aa19718be39f41
dave <djungst@gmail.com>
2016-11-08 15:19:37 -0700

    forgot deliver method

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -11,7 +11,7 @@ class ContactController < ApplicationController
   def request_info
     contact = params[:contact]
     begin
-      ContactMailer.contact(contact[:name], contact[:email], contact[:content])
+      ContactMailer.contact(contact[:name], contact[:email], contact[:content]).deliver
       flash[:notice] = "Your message has been sent"
     rescue => e
       logger.error e.message
